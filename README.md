7cecb79ea50b0a1cbd3ee107a7b9cef76ae44edb
wilgnerl <wilgnerl@al.insper.edu.br>
2023-07-01 18:26:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# clubapp-django-project
---
+++ events/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Venue, Event, MyClubUser
+
+# Register your models here.
+# admin.site.register(Venue)
+# admin.site.register(Event)
+admin.site.register(MyClubUser)
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'venue'), 'event_date', 'description', 'manager',)
+    list_display = ('name', 'event_date', 'venue',)
+    list_filter = ('event_date', 'venue',)
+    ordering = ('-event_date',)
